d09c6cdeca85e7b78501b47de9610bd6da65fb4c
Leye <wangleye@gmail.com>
2017-09-27 20:39:44 +0800

    read data from db with pandas

--- gender_prediction_FB.py
+++ gender_prediction_FB.py
@@ -1,40 +1,22 @@
 import pymysql
+import pandas as pd
 
 conn = pymysql.connect(host= "localhost",
                   user="root",
                   passwd="123456",
                   db="all0504")
 
-workEdu = {}
-work_cc = {}
-def find_users(query_types):
-	query_str = ', '.join(query_types)
-	query_statement = "SELECT iduser, gender, musicstr, moviestr, bookstr, televisionstr, gamestr FROM user where gender != 0 and content>0".format(query_str)
-	x = conn.cursor()
-	x.execute(query_statement)
-	results = x.fetchall()
-	print 'start count results...'
-	for result in results:
-		user_id = result[0]
-		if user_id not in u_cc_map:
-			continue
-		
-		### store all the we
-		result_str = ''
-		for idx in range(1, len(query_types)):
-			if result[idx].strip() == '' or result[idx].strip() == '""':
-				continue
-			result_str += result[idx] + ','
-		if result_str == '':
-			continue
-		recent_we = result_str.split(',')[0]
-		workEdu[user_id] = recent_we
-		if recent_we not in work_cc:
-			work_cc[recent_we] = set()
-		work_cc[recent_we].add(u_cc_map[user_id])
+def find_users():
+	query_statement = "SELECT iduser, gender, musicstr, moviestr, bookstr, televisionstr, gamestr FROM user where gender != 0 and content>0"
+	users_gender = pd.read_sql_query(query_statement, conn)
 
-		#### only store recent we
-		# if entities[0] not in workEdu: # only keep the recent work/education
-		# 	workEdu[entities[0]] = 1
-		# else:
-		# 	workEdu[entities[0]] += 1
+	print(users_gender.sample(5))
+	users_gender.to_pickle('./data/users_with_gender_interests.pkl')
+
+
+def load_users():
+	return pd.read_pickle('./data/users_with_gender_interests.pkl')
+
+if __name__ == "__main__":
+	users = load_users()
+	print(users.describe())
