c3b57bc16b58ef534bc3e30573823ef132a39d9d
Leye <wangleye@gmail.com>
2017-09-28 08:10:20 +0800

    Add prediction models

--- gender_prediction_FB.py
+++ gender_prediction_FB.py
@@ -1,66 +1,29 @@
-import pymysql
 import pickle
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
 
-conn = pymysql.connect(host= "localhost",
-                  user="root",
-                  passwd="123456",
-                  db="all0504")
+features = ['musicscore', 'moviescore', 'bookscore', 'televisionscore', 'gamescore']
 
-interest_names = ['music', 'movie', 'book', 'television', 'game']
-
-def save_obj(obj, name):
-    with open('data/'+ name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-def load_obj(name):
-    with open('data/' + name + '.pkl', 'rb') as f:
-        return pickle.load(f)
-
-def find_users():
-	query_statement = "SELECT iduser, gender, musicstr, moviestr, bookstr, televisionstr, gamestr FROM user where gender != 0 and content>0"
-	users_gender = pd.read_sql_query(query_statement, conn)
-
-	print(users_gender.sample(5))
-	users_gender.to_pickle('./data/users_with_gender_interests.pkl')
-
-
-def preprocessing():
-	users_df = load_users_raw()
-	for interest in interest_names:
-		users_df[interest+'list'] = [[]]*len(users_df)
-	for index, row in users_df.iterrows():
-		for interest in interest_names:
-			interest_items = row[interest+'str'].split(';')
-			users_df.at[index, interest+'list'] = interest_items
-	print(users_df.sample(5))
-	users_df.to_pickle('./data/users_with_gender_interest_array.pkl')
-
-
-def load_users_raw():
-	return pd.read_pickle('./data/users_with_gender_interests.pkl')
-
-
-def load_users():
-	return pd.read_pickle('./data/users_with_gender_interest_array.pkl')  # added 'musiclist', 'movielist', etc. to store interest list in the df
-
-
-def generate_interest_indication_matrix():
-	dict_interest = {}
-	users_df = load_users()
-	for index, row in users_df.iterrows():
-		gender = row['gender']
-		for interest in interest_names:
-			interest_items = row[interest+'list']
-			for interest_i in interest_items:
-				if interest_i not in dict_interest:
-					dict_interest[interest_i] = {}
-					dict_interest[interest_i][1] = 0
-					dict_interest[interest_i][2] = 0
-				dict_interest[interest_i][gender] += 1
-	save_obj(dict_interest, 'interest_gender_indication')
-
-
+def load_users_with_interest_scores():
+	return pd.read_pickle('./data/users_with_gender_interest_score.pkl')
 
 if __name__ == "__main__":
-	generate_interest_indication_matrix()
+	users_df = load_users_with_interest_scores()
+	print(users_df.sample(10))
+	print("total number of users: ", len(users_df))
+
+	y = users_df['gender']
+	X = users_df[features]
+
+	lr = LogisticRegression()
+	rf = RandomForestClassifier()
+	svc = SVC()
+
+	classifier_models = [lr, rf, svc]
+
+	for model in classifier_models:
+		print(cross_val_score(model, X, y, cv=5))
---
+++ gender_prediction_FB_data_preprocessing - 副本.py
@@ -0,0 +1,102 @@
+import pymysql
+import pickle
+import numpy as np
+import pandas as pd
+
+conn = pymysql.connect(host= "localhost",
+                  user="root",
+                  passwd="123456",
+                  db="all0504")
+
+interest_names = ['music', 'movie', 'book', 'television', 'game']
+interest_ind = {}
+
+def save_obj(obj, name):
+    with open('data/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open('data/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+def find_users():
+	query_statement = "SELECT iduser, gender, musicstr, moviestr, bookstr, televisionstr, gamestr FROM user where gender != 0 and content>0"
+	users_gender = pd.read_sql_query(query_statement, conn)
+
+	print(users_gender.sample(5))
+	users_gender.to_pickle('./data/users_with_gender_interests.pkl')
+
+def preprocessing():
+	users_df = load_users_raw()
+	for interest in interest_names:
+		users_df[interest+'list'] = [[]]*len(users_df)
+	for index, row in users_df.iterrows():
+		for interest in interest_names:
+			interest_items = row[interest+'str'].split(';')
+			users_df.at[index, interest+'list'] = interest_items
+	print(users_df.sample(5))
+	users_df.to_pickle('./data/users_with_gender_interest_array.pkl')
+
+def load_users_raw():
+	return pd.read_pickle('./data/users_with_gender_interests.pkl')
+
+def generate_interest_indication_matrix():
+	dict_interest = {}
+	users_df = load_users()
+	for index, row in users_df.iterrows():
+		gender = row['gender']
+		for interest in interest_names:
+			interest_items = row[interest+'list']
+			for interest_i in interest_items:
+				if interest_i not in dict_interest:
+					dict_interest[interest_i] = {}
+					dict_interest[interest_i][1] = 0
+					dict_interest[interest_i][2] = 0
+				dict_interest[interest_i][gender] += 1
+	save_obj(dict_interest, 'interest_gender_indication')
+
+def generate_user_interest_feature():
+	users_df = load_users()
+	for interest in interest_names:
+		users_df[interest+'score'] = -1.0
+	for index, row in users_df.iterrows():
+		for interest in interest_names:
+			score = []
+			for interest_i in row[interest+'list']:
+				i_s = interest_item_to_gender(interest_i)
+				if i_s is not None:
+					score.append(i_s)
+			if len(score) > 0:
+				users_df.at[index, interest+'score'] = np.mean(score)
+	for interest in interest_names:
+		del users_df[interest+'str']
+		del users_df[interest+'list']
+	print(users_df.sample(5))
+	users_df.to_pickle('./data/users_with_gender_interest_score.pkl')
+
+def load_users():
+	return pd.read_pickle('./data/users_with_gender_interest_array.pkl')  # added 'musiclist', 'movielist', etc. to store interest list in the df
+
+def load_interest_indication_dict():
+	return load_obj('interest_gender_indication')
+
+def load_users_with_interest_scores():
+	return pd.read_pickle('./data/users_with_gender_interest_score.pkl')
+
+def interest_item_to_gender(interest_i, min_count=5): # min_count: the interest whose liked user number smaller than it will not be considered
+	if interest_i not in interest_ind:
+		return None
+	gender1 = interest_ind[interest_i][1]
+	gender2 = interest_ind[interest_i][2]
+	total_liked_user_num = gender1 + gender2
+	if total_liked_user_num < min_count:
+		return None
+	else:
+		return gender1*1.0/total_liked_user_num
+
+
+if __name__ == "__main__":
+	interest_ind = load_interest_indication_dict()
+	users_df = load_users_with_interest_scores()
+	print(users_df.sample(10))
+
